261f754d205a56408d430093a87a05dc82f53cfc
DSOTM-RSA <eoe.ddg.deu@gmail.com>
2016-12-13 13:21:28 +0100

    refs for ftp access added

--- NCEI/isd_parser.R
+++ NCEI/isd_parser.R
@@ -2,3 +2,8 @@ library(isdparser)
 
 path <- ('3391047210197dat.txt')
 reader.in<-isd_parse(path,progress = TRUE)
+
+
+ftp://ftp.ncdc.noaa.gov/pub/data/gsod/2016/ # ftp of aggregated daily ISD data for 9000 stations
+  ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite # ftp of lite version of entire ISD dataset..
+ftp://ftp.ncdc.noaa.gov/pub/data/noaa # full version of  ISD data
